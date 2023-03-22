319e47c3b40555616d11a2101ca8af4bfcb319e0
am0654 <94629609+am0654@users.noreply.github.com>
2023-03-22 16:52:18 +0530

    Create SearchingAElement.cpp

---
+++ SearchingAElement.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+ 
+int search(int arr[], int N, int x)
+{
+    int i;
+    for (i = 0; i < N; i++)
+        if (arr[i] == x)
+            return i;
+    return -1;
+}
+ 
+// Driver's code
+int main(void)
+{
+    int arr[] = { 5, 30, 41, 10, 40 };
+    int x = 10;
+    int N = sizeof(arr) / sizeof(arr[0]);
+ 
+    // Function call
+    int result = search(arr, N, x);
+    (result == -1)
+        ? cout << "Element is not present in array"
+        : cout << "Element is present at index " << result;
+    return 0;
+}
+
+Output:Element is present at index 3
