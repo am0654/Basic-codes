0d97b07ad23e622d8c75567615b1c6c469e4e90d
am0654 <94629609+am0654@users.noreply.github.com>
2023-03-28 23:45:37 +0530

    Create ReverseAnumber.cpp

---
+++ ReverseAnumber.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num, rem, rev=0;
+    cout<<"Enter the Number: ";
+    cin>>num;
+    while(num!=0)
+    {
+        rem = num%10;
+        rev = rem + (rev*10);
+        num = num/10;
+    }
+    cout<<"\nReverse = "<<rev;
+    cout<<endl;
+    return 0;
+}
+
+
+Input:
+Enter the Number: 49
+Output:
+Reverse = 94
