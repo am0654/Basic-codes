5c587d1b9c9bdbd6843e2366795ea4ff6b4869af
am0654 <94629609+am0654@users.noreply.github.com>
2023-03-28 23:30:36 +0530

    Create CheckLeapYearOrNot.cpp

---
+++ CheckLeapYearOrNot.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int year;
+    cout<<"Enter the Year: ";
+    cin>>year;
+    if((year%4==0) && (year%100!=0))
+        cout<<"\nIt is a Leap Year";
+    else if(year%400==0)
+        cout<<"\nIt is a Leap Year";
+    else
+        cout<<"\nIt is not a Leap Year";
+    cout<<endl;
+    return 0;
+}
+
+
+Input:
+Enter the Year: 2020
+Output:
+It is a Leap Year
