4e6ec8f1eec46653f97baabc9f22dd6daf006125
am0654 <94629609+am0654@users.noreply.github.com>
2023-04-05 23:11:59 +0530

    Create PerimeterOfSquare.cpp

---
+++ PerimeterOfSquare.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float len, per;
+    cout<<"Enter the Length of Square: ";
+    cin>>len;
+    per = 4*len;
+    cout<<"\nPerimeter = "<<per;
+    cout<<endl;
+    return 0;
+}
+
+
+Input:
+Enter the Length of Square: 5.5
+Output:
+Perimeter = 22
