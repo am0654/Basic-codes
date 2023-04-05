fa87459cfd8ad393ea953c71673c70f4cd9960a6
am0654 <94629609+am0654@users.noreply.github.com>
2023-04-05 23:20:21 +0530

    Create PerimeterOfRectangle.cpp

---
+++ PerimeterOfRectangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float len, bre, per;
+    cout<<"Enter Length and Breadth of Rectangle: ";
+    cin>>len>>bre;
+    per = 2*(len+bre);
+    cout<<"\nPerimeter = "<<per;
+    cout<<endl;
+    return 0;
+}
+
+
+
+Input:
+Enter Length and Breadth of Rectangle: 4 5
+Output:
+Perimeter = 18
