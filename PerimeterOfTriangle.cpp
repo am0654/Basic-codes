7ebb829aa7a2cbdeef59a42990f0957784470df8
am0654 <94629609+am0654@users.noreply.github.com>
2023-04-06 23:40:04 +0530

    Create PerimeterOfTriangle.cpp

---
+++ PerimeterOfTriangle.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float s1,s2,s3,peri;
+    cout<<"Enter first side of triangle: ";
+    cin>>s1;
+    cout<<"Enter second side of triangle: ";
+    cin>>s2;
+    cout<<"Enter third side of triangle: ";
+    cin>>s3;
+    peri=s1+s2+s3;
+    cout<<"Perimeter of Triangle is: "<<peri;
+    return 0;
+}
+
+
+Input:
+Enter first side of triangle: 12.5
+Enter second side of triangle: 15.5
+Enter third side of triangle: 20.2
+  
+  
+Output:
+Perimeter of Triangle is: 48.2
