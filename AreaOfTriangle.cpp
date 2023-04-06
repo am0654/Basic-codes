e367d811d4f16876019d50ec9ae1b9860f12f067
am0654 <94629609+am0654@users.noreply.github.com>
2023-04-06 23:45:06 +0530

    Create AreaOfTriangle.cpp

---
+++ AreaOfTriangle.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float b, h, area;
+    cout<<"Enter Base length of Triangle: ";
+    cin>>b;
+    cout<<"Enter Height length of Triangle: ";
+    cin>>h;
+    area = 0.5*b*h;
+    cout<<"\nArea = "<<area;
+    cout<<endl;
+    return 0;
+}
+
+
+Input:
+Enter Base length of Triangle: 20.2
+Enter Height length of Triangle: 15.6
+
+Output:
+Area = 157.56
