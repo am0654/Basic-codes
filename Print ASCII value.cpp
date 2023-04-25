45b73b37179670989aa59cb209ee597d93ff6112
am0654 <94629609+am0654@users.noreply.github.com>
2023-04-25 23:10:43 +0530

    Create Print ASCII value.cpp

---
+++ Print ASCII value.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    int i;
+    cout<<"Enter the Character: ";
+    cin>>ch;
+    i = ch;
+    cout<<"\nASCII Value = "<<i;
+    cout<<endl;
+    return 0;
+}
+
+
+
+Input:
+Enter the Character: A
+
+Output:
+ASCII Value = 65
